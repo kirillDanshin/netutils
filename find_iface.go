6fbc534a3776c7728dce7bc56ce6732b48d0870f
Kirill Danshin <k@guava.by>
2016-07-11 11:33:59 +0300

    add withCaller

--- find_iface.go
+++ find_iface.go
@@ -6,29 +6,58 @@ import (
 	"sync"
 
 	"github.com/kirillDanshin/dlog"
+	"github.com/kirillDanshin/myutils"
 )
 
 // FindIfaceWithAddr finds an interface
 // with a given address. returns empty string and error
 // if not found or error happened.
-func FindIfaceWithAddr(addr string) (string, error) {
+func FindIfaceWithAddr(addr string, withCaller ...bool) (string, error) {
+	var wCaller bool
+	if len(withCaller) > 0 {
+		wCaller = withCaller[0]
+	}
 	ifaceName := ""
 	check := func(iface *net.Interface) {
 		addrs, err := iface.Addrs()
 		if err != nil {
-			dlog.F("err: %s", err)
+			if wCaller {
+				dlog.F("%s: [\n\terr: %s\n]", myutils.Slice(dlog.GetCaller(2))[0].(*dlog.Caller).String(), err)
+			} else {
+				dlog.F("err: %s", err)
+			}
 			return
 		}
 		iface.Addrs()
 		for _, ifaceAddr := range addrs {
-			dlog.F("ifaceAddr=[%#+v] addr=[%#+v]", ifaceAddr.String(), addr)
+			if wCaller {
+				dlog.F(
+					"%s: [\n\tifaceAddr=[%#+v]\n\taddr=[%#+v]\n]",
+					myutils.Slice(dlog.GetCaller(2))[0].(*dlog.Caller).String(),
+					ifaceAddr.String(),
+					addr,
+				)
+			} else {
+				dlog.F("ifaceAddr=[%#+v] addr=[%#+v]", ifaceAddr.String(), addr)
+			}
 			_, cidrnet, err := net.ParseCIDR(ifaceAddr.String())
 			if err != nil {
-				dlog.F("Error: %s", err)
+				if wCaller {
+					dlog.F("%s: [\n\tError: %s\n]", myutils.Slice(dlog.GetCaller(2))[0].(*dlog.Caller).String(), err)
+				} else {
+					dlog.F("Error: %s", err)
+				}
 				return
 			}
 			myaddr := net.ParseIP(addr)
-			dlog.F("contains=[%#+v]", cidrnet.Contains(myaddr))
+			if wCaller {
+				dlog.F("%s: [\n\tcontains=[%#+v]\n]",
+					myutils.Slice(dlog.GetCaller(2))[0].(*dlog.Caller).String(),
+					cidrnet.Contains(myaddr),
+				)
+			} else {
+				dlog.F("contains=[%#+v]", cidrnet.Contains(myaddr))
+			}
 			if cidrnet.Contains(myaddr) {
 				ifaceName = iface.Name
 			}
